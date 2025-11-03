48330f69283f428471663458d6831d7e4ab506b9
Preshit Dagamwar <preshitdagamwar152@gmail.com>
2025-11-04 00:04:42 +0530

    Delete react-practise directory

--- react-practise/src/components/section-1/pages/Inva.jsx
+++
@@ -1,9 +0,0 @@
-const Inva = () => {
-  return (
-    <div className="flex items-center justify-center  h-[100vh]">
-      <h1 className="text-8xl text-white font-bold">404 Invalid</h1>
-    </div>
-  );
-};
-
-export default Inva;
